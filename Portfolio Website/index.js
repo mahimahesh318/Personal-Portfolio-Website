dbaf54cfdb80ad32da7971f618c7819d9b362e35
Mankali Mahesh <112609869+mahimahesh318@users.noreply.github.com>
2025-06-26 22:26:14 +0530

    Add files via upload

---
+++ Portfolio Website/index.js
@@ -0,0 +1,16 @@
+
+let linkedin = ()=>{
+  window.location.href="https://www.linkedin.com/in/mankali-mahesh/";
+}
+
+let github = ()=>{
+  window.location.href="https://github.com/mahimahesh318";
+}
+
+let resume = ()=>{
+  window.location.href="https://www.canva.com/design/DAGcEaX0mI4/7QtojgTwVjG6eGqjv6RKAQ/view?utm_content=DAGcEaX0mI4&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=ha691cf42ac";
+}
+
+let git = () =>{
+  window.location.href="https://github.com/mahimahesh318/OneGo";
+}
